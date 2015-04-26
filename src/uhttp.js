31219d9025372ec581012e05de18127eed269057
Suyog Sonwalkar <flux159@gmail.com>
2015-04-26 14:37:32 -0400

    Fixing up small issue with client side uhttp and module.exports.

--- src/uhttp.js
+++ src/uhttp.js
@@ -4,8 +4,8 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define(factory);
-    } else if (typeof 'exports' === 'object') {
-        module.exports = factory;
+    } else if (typeof module !== 'undefined') {
+        module.exports = factory();
     } else {
         root.uhttp = factory(root);
     }
